0a5c8ae0a504cc04061f1402c4792e9c9a1ee495
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2025-05-01 18:27:04 +0600

    remove anim

--- app/Blogs/(components)/Card.jsx
+++ app/Blogs/(components)/Card.jsx
@@ -3,8 +3,7 @@ import CardText from "./CardText"
 
 const Card = ({ colSpanTwo, tag, image = '/blogs/blog-1.png', title, desc, minsToRead, link = "/#" }) => {
     return (
-
-        <div className={`card-items font-urbanist relative border-2 border-[#686868] rounded-[16px] md:relative ${colSpanTwo ? "md:col-span-2" : ""} `}>
+        <div className={`font-urbanist relative border-2 border-[#686868] rounded-[16px] md:relative ${colSpanTwo ? "md:col-span-2" : ""} `}>
             <span className="absolute top-4 left-4 font-medium  bg-white text-black px-4 py-2.5 !leading-0 rounded-full text-sm md:text-xl xl:text-[15px] ">
                 {tag}
             </span>
@@ -13,7 +12,6 @@ const Card = ({ colSpanTwo, tag, image = '/blogs/blog-1.png', title, desc, minsT
             </div>
             <CardText colSpanTwo={colSpanTwo} title={title} desc={desc} minsToRead={minsToRead} link={link} />
         </div>
-
     )
 }
 
