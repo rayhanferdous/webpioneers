716a92bccb20a21439d0d9a09fdca36fb703ceb2
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2025-05-01 17:36:08 +0600

    add amination on blog index

--- app/Blogs/page.jsx
+++ app/Blogs/page.jsx
@@ -1,4 +1,5 @@
 import { Badge } from "../(components)/ui/Badge";
+import MountAnim from "../(components)/ui/MountAnim";
 import Tags from "../(components)/ui/Tags";
 import { blogs } from "../(constants)/blogs";
 import Card from "./(components)/Card";
@@ -10,16 +11,27 @@ const Blogs = () => {
         <div className="headerSpace"></div>
         <div className="flex flex-col sm:items-start sm:justify-start px-con">
           <div className="flex flex-col items-start sm:text-center sm:justify-start gap-4">
-            <Badge nodot variant="yellow">
-              Blogs
-            </Badge>
-            <h1 className="text-4xl sm:text-[3.5rem] leading-tight mx-auto max-w-[36ch] 2xl:max-w-[32ch]">
-              Thoughts That Matter
-            </h1>
+            <MountAnim>
+              <Badge nodot variant="yellow">
+                Blogs
+              </Badge>
+            </MountAnim>
+            <MountAnim>
+              <h1 className="text-4xl sm:text-[3.5rem] leading-tight mx-auto max-w-[36ch] 2xl:max-w-[32ch]">
+                Thoughts That Matter
+              </h1>
+            </MountAnim>
           </div>
           <div className="mt-14 flex items-center gap-4">
-            <Tags title="All" active={true} />
-            <Tags title="Web Development" />
+            <MountAnim>
+              <Tags title="All" active={true} />
+            </MountAnim>
+            <MountAnim>
+              <Tags title="Design" />
+            </MountAnim>
+            <MountAnim>
+              <Tags title="Web Development" />
+            </MountAnim>
           </div>
           <div className="mt-6 sm:mt-14 border-b border-gray w-full"></div>
         </div>
@@ -27,16 +39,18 @@ const Blogs = () => {
       {/* card section */}
       <div className="grid gap-y-7 pt-10 md:pt-[42px] lg:pt-14 md:gap-y-10 xl:gap-y-14 2xl:gap-y-20 gap-x-9 md:gap-x-9 xl:gap-x-8 2xl:gap-x-8 sm:grid-cols-2 md:grid-cols-3 px-con">
         {blogs.map((blog, index) => (
-          <Card
-            key={index}
-            tag={blog.tag}
-            image={blog.thumbnail}
-            title={blog.title}
-            colSpanTwo={blog.col_span_two}
-            desc={blog.description[0].substring(0, 150)}
-            minsToRead={blog.time_to_read}
-            link={blog.link}
-          />
+          <MountAnim>
+            <Card
+              key={index}
+              tag={blog.tag}
+              image={blog.thumbnail}
+              title={blog.title}
+              colSpanTwo={blog.col_span_two}
+              desc={blog.description[0].substring(0, 150)}
+              minsToRead={blog.time_to_read}
+              link={blog.link}
+            />
+          </MountAnim>
         ))}
       </div>
     </>
