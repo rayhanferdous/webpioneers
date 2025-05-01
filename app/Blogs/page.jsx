32f82abbfe90552243422fcdfd1f7d2c9144b7b1
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2025-05-01 17:41:31 +0600

    fix animaton related problem on blog page

--- app/Blogs/page.jsx
+++ app/Blogs/page.jsx
@@ -39,7 +39,7 @@ const Blogs = () => {
       {/* card section */}
       <div className="grid gap-y-7 pt-10 md:pt-[42px] lg:pt-14 md:gap-y-10 xl:gap-y-14 2xl:gap-y-20 gap-x-9 md:gap-x-9 xl:gap-x-8 2xl:gap-x-8 sm:grid-cols-2 md:grid-cols-3 px-con">
         {blogs.map((blog, index) => (
-          <MountAnim>
+          blog.col_span_two ?
             <Card
               key={index}
               tag={blog.tag}
@@ -50,7 +50,18 @@ const Blogs = () => {
               minsToRead={blog.time_to_read}
               link={blog.link}
             />
-          </MountAnim>
+            : <MountAnim>
+              <Card
+                key={index}
+                tag={blog.tag}
+                image={blog.thumbnail}
+                title={blog.title}
+                colSpanTwo={blog.col_span_two}
+                desc={blog.description[0].substring(0, 150)}
+                minsToRead={blog.time_to_read}
+                link={blog.link}
+              />
+            </MountAnim>
         ))}
       </div>
     </>
