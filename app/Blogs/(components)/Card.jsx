0abc67a292d0e4d5bf9250cc48dd50681553b29d
Rayhan Ferdous <107350448+rayhanferdous@users.noreply.github.com>
2025-05-01 12:26:53 +0600

    fix responsive issues

--- app/Blogs/(components)/Card.jsx
+++ app/Blogs/(components)/Card.jsx
@@ -7,7 +7,9 @@ const Card = ({ colSpanTwo, tag, image = '/blogs/blog-1.png', title, desc, minsT
             <span className="absolute top-4 left-4 font-medium  bg-white text-black px-4 py-2.5 !leading-0 rounded-full text-sm md:text-xl xl:text-[15px] ">
                 {tag}
             </span>
-            <Image className={`${colSpanTwo ? "md:min-h-full" : ""} max-md:h-full max-md:max-h-[252px] min-w-full rounded-[14px]`} src={image} width={539} height={359} alt={title} />
+            <div className="w-full  md:max-h-[252px] ">
+                <Image className={`${colSpanTwo ? "md:min-h-full" : ""} h-full min-w-full rounded-[14px]`} src={image} width={539} height={359} alt={title} />
+            </div>
             <CardText colSpanTwo={colSpanTwo} title={title} desc={desc} minsToRead={minsToRead} link={link} />
         </div>
     )
--- app/Blogs/(components)/CardText.jsx
+++ app/Blogs/(components)/CardText.jsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const CardText = ({ colSpanTwo, title, desc, minsToRead, link = "/#" }) => {
     return (
-        <div className={`${colSpanTwo && 'md:absolute md:bottom-9 md:left-9 md:bg-white max-w-[400px] 2xl:max-w-[549px] rounded-2xl'} p-6 flex flex-col gap-[18px] md:gap-6 lg:gap-4 2xl:gap-9`}>
+        <div className={`${colSpanTwo && 'min-[1024px]:absolute min-[1024px]:bottom-9 min-[1024px]:left-9 min-[1024px]:bg-white max-w-[400px] 2xl:max-w-[549px] rounded-2xl'} p-6 flex flex-col gap-[18px] md:gap-6 lg:gap-4 2xl:gap-9`}>
             <div className="flex flex-col gap-4 md:gap-5">
                 <h3 className={`text-[21px] md:text-[18px] lg:text-[21px] 2xl:text-[28px]"} font-medium text-dark`}>{title}</h3>
                 <p className="font-medium text-[#7A7A7A] text-[18px] lg:text-[16px] xl:text-[15px] 2xl:text-[20px]">
@@ -14,7 +14,7 @@ const CardText = ({ colSpanTwo, title, desc, minsToRead, link = "/#" }) => {
                 <Link href={link}>
                     <svg width="49" height="49" viewBox="0 0 49 49" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <circle cx="24.5" cy="24.5" r="24.5" fill="#1D1D1D" />
-                        <path d="M17.4297 25H31.5718M31.5718 25L24.5008 17.9289M31.5718 25L24.5008 32.0711" stroke="white" stroke-width="2" strokeLinecap="round" strokeLinejoin="round" />
+                        <path d="M17.4297 25H31.5718M31.5718 25L24.5008 17.9289M31.5718 25L24.5008 32.0711" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                     </svg>
                 </Link>
             </div>
